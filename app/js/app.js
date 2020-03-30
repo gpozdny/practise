c4acf359809bcee2538af4cb7bf8917f9fa968fb
Gennady Yudin <soulmatesneverdie92@gmail.com>
2020-03-31 02:30:58 +0400

    hi

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-	// Custom JS
+	var mySwiper = new Swiper('.swiper-container', {
+		speed: 1000,
+		spaceBetween: 100,
+		centeredSlides: true,
+		navigation: {
+			nextEl: '.fa-caret-right',
+			prevEl: '.fa-caret-left'
+		},
+		autoplay: {
+			delay: 4000,
+		},
+		loop: true,
+
+		watchOverflow: true,
+		setWrapperSize: true
+
+	});
 
 });
