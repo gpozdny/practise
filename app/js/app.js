c938c9dc332652970eef63e11d7a1dc29d251145
Gennady Yudin <soulmatesneverdie92@gmail.com>
2020-04-01 20:02:05 +0400

    innas

--- app/js/app.js
+++ app/js/app.js
@@ -18,4 +18,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	});
 
+	// POPUP
+
+	let buttonPopUp = document.querySelector('.login__button'),
+		overlay = document.getElementById('overlay'),
+		popUp = document.getElementById('popUp'),
+		crossPopUp = document.getElementById('cross');
+
+	buttonPopUp.addEventListener('click', function(){
+		showPopUp();
+	});
+
+	crossPopUp.addEventListener('click', function(){
+		hidePopUp();
+	});
+
+	function showPopUp() {
+		overlay.classList.remove('isHidden'),
+		overlay.classList.add('isShown');
+		popUp.classList.remove('isHidden'),
+		popUp.classList.add('isShown');
+		// document.body.classList.add('scrollIsBlocked');
+	}
+
+	function hidePopUp() {
+		overlay.classList.remove('isShown');
+		popUp.classList.remove('isShown');
+		// document.body.classList.remove('scrollIsBlocked');
+	}
+
+	// Check student ID
+
+	let form = document.getElementById('submit'),
+		inputValue = document.getElementById('value');
+
+	form.addEventListener('click', function(){
+		let studentID = inputValue.value;
+
+		switch (studentID) {
+			case '1nna': 
+			alert(`RIGHT!`);
+			break;
+			default:
+				alert(`there is no such ID`);
+				event.preventDefault();
+		}
+	});
 });
